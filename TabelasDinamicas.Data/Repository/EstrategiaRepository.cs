f72cae3cee4cd0288a4d4aed5ddb91a763d9a3c5
Pedro <pedro.augusto77@hotmail.com>
2023-10-16 09:54:21 -0300

    arquivos criados

---
+++ TabelasDinamicas.Data/Repository/EstrategiaRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabelasDinamicas.Domain.Interfaces;
+
+namespace TabelasDinamicas.Data.Repository
+{
+    public class EstrategiaRepository : IEstrategiaRepository
+    {
+    }
+}
---
+++ TabelasDinamicas.Domain/Interfaces/IRegistroRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabelasDinamicas.Domain.Interfaces
+{
+    public interface IRegistroRepository
+    {
+    }
+}
