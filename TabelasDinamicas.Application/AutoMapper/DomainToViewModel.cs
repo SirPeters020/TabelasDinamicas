3a3c935d313b7dbd79871230fa282c6a2e9b5930
Pedro <pedro.augusto77@hotmail.com>
2023-12-05 17:31:04 -0300

    ajustes

---
+++ TabelasDinamicas.Application/AutoMapper/DomainToViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TabelasDinamicas.Application.ViewModel;
+using TabelasDinamicas.Domain.Model;
+
+namespace TabelasDinamicas.Application.AutoMapper;
+
+public class DomainToViewModel : Profile
+{
+    public DomainToViewModel()
+    {
+
+        CreateMap<Estrategia, EstrategiaViewModel>();
+
+        CreateMap<Registros, RegistrosViewModel>();
+
+        CreateMap<Complemento, ComplementoResumeViewModel>();
+
+    }
+}
